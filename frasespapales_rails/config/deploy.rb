4af09f80cf74fc40b0ed846818aaf738e6d13c4b
Damian Ferrai <damianferrai@gmail.com>
2013-03-23 16:09:50 -0300

    Adding index

--- frasespapales_rails/config/deploy.rb
+++ frasespapales_rails/config/deploy.rb
@@ -1,14 +1,26 @@
 # This is a sample Capistrano config file for EC2 on Rails.
 # It should be edited and customized.
 
-require "ec2onrails"
 require "rvm/capistrano"                  # Load RVM's capistrano plugin.
 
 set :application, "frasespapales"
 
+# The directory on the EC2 node that will be deployed to
+set :deploy_to, "/var/www/#{application}"
+
+# Your EC2 instances. Use the ec2-xxx....amazonaws.com hostname, not
+# any other name (in case you have your own DNS alias) or it won't
+# be able to resolve to the internal IP address.
+role :web,      "ec2-54-232-196-174.sa-east-1.compute.amazonaws.com"
+role :memcache, "ec2-54-232-196-174.sa-east-1.compute.amazonaws.com"
+role :db,       "ec2-54-232-196-174.sa-east-1.compute.amazonaws.com", :primary => true
+
+default_run_options[:pty] = true
+
+
 set :user, "ec2-user"
-set :runner, "ec2-user"
-set :admin_runner, "ec2-user"
+#set :use_sudo, false
+set :admin_runner, "admin"
 
 set :ssh_options, {:user => "ec2-user" }
 
@@ -17,122 +29,48 @@ set :ssh_options, {:keys => ["/Users/damianferrai/.ssh/da"]}
 set :ssh_options, {:forward_agent => true }
 set :ssh_options, {:verbose => :debug}
 
-default_run_options[:pty] = true
-
 # NOTE: for some reason Capistrano requires you to have both the public and
 # the private key in the same folder, the public key should have the 
 # extension ".pub".
 #ssh_options[:keys] = ["/Users/damianferrai/.ssh/id_rsa"]
 
 
-### REPO
-set :repository, "git@github.com:dalef84/https://github.com/frasespapales.git"  # Your clone URL
-set :scm, "git"
+#############################################################
+#	Git SCM
+#############################################################
+
+set :repository, "git@github.com:dalef84/frasespapales.git"  # Your clone URL
+set :scm, :git
 set :branch, "master"
 set :deploy_via, :remote_cache
-
-
-
-# Your EC2 instances. Use the ec2-xxx....amazonaws.com hostname, not
-# any other name (in case you have your own DNS alias) or it won't
-# be able to resolve to the internal IP address.
-role :web,      "ec2-user@ec2-54-232-196-174.sa-east-1.compute.amazonaws.com"
-role :memcache, "ec2-user@ec2-54-232-196-174.sa-east-1.compute.amazonaws.com"
-role :db,       "ec2-user@ec2-54-232-196-174.sa-east-1.compute.amazonaws.com", :primary => true
-# role :db,       "ec2-56-xx-xx-xx.z-1.compute-1.amazonaws.com", :primary => true, :ebs_vol_id => 'vol-12345abc'
-# optinally, you can specify Amazon's EBS volume ID if the database is persisted 
-# via Amazon's EBS.  See the main README for more information.
 
 # Whatever you set here will be taken set as the default RAILS_ENV value
 # on the server. Your app and your hourly/daily/weekly/monthly scripts
 # will run with RAILS_ENV set to this value.
 set :rails_env, "production"
 
-# EC2 on Rails config. 
-# NOTE: Some of these should be omitted if not needed.
-set :ec2onrails_config, {
-  # S3 bucket and "subdir" used by the ec2onrails:db:restore task
-  # NOTE: this only applies if you are not using EBS
-  :restore_from_bucket => "frasespapales",
-  :restore_from_bucket_subdir => "database",
-  
-  # S3 bucket and "subdir" used by the ec2onrails:db:archive task
-  # This does not affect the automatic backup of your MySQL db to S3, it's
-  # just for manually archiving a db snapshot to a different bucket if 
-  # desired.
-  # NOTE: this only applies if you are not using EBS
-  :archive_to_bucket => "frasespapales",
-  :archive_to_bucket_subdir => "db-archive/#{Time.new.strftime('%Y-%m-%d--%H-%M-%S')}",
-  
-  # Set a root password for MySQL. Run "cap ec2onrails:db:set_root_password"
-  # to enable this. This is optional, and after doing this the
-  # ec2onrails:db:drop task won't work, but be aware that MySQL accepts 
-  # connections on the public network interface (you should block the MySQL
-  # port with the firewall anyway). 
-  # If you don't care about setting the mysql root password then remove this.
-  :mysql_root_password => "your-mysql-root-password",
-  
-  # Any extra Ubuntu packages to install if desired
-  # If you don't want to install extra packages then remove this.
-  :packages => ["logwatch", "imagemagick"],
-  
-  # Any extra RubyGems to install if desired: can be "gemname" or if a 
-  # particular version is desired "gemname -v 1.0.1"
-  # If you don't want to install extra rubygems then remove this
-  # NOTE: if you are using rails 2.1, ec2onrails calls 'sudo rake gem:install',
-  # which will install gems defined in your rails configuration
-  :rubygems => [],
-  
-  # extra security measures are taken if this is true, BUT it makes initial
-  # experimentation and setup a bit tricky.  For example, if you do not
-  # have your ssh keys setup correctly, you will be locked out of your
-  # server after 3 attempts for upto 3 months.  
-  :harden_server => false,
-  
-  #if you want to harden the server, or setup email signing, you will need to set the domain
-  #if you use Capistrano's multistage extension (recommended!), you can add a line like this to your
-  #environment specific file:
-  #      ec2onrails_config[:service_domain] = 'staging.mydomain.com'
-  :service_domain => nil,
-  
-  # Set the server timezone. run "cap -e ec2onrails:server:set_timezone" for 
-  # details
-  :timezone => "UTC",
-  
-  # Files to deploy to the server (they'll be owned by root). It's intended
-  # mainly for customized config files for new packages installed via the 
-  # ec2onrails:server:install_packages task. Subdirectories and files inside
-  # here will be placed in the same structure relative to the root of the
-  # server's filesystem. 
-  # If you don't need to deploy customized config files to the server then
-  # remove this.
-  #:server_config_files_root => "../server_configs",
-  
-  # If config files are deployed, some services might need to be restarted.
-  # If you don't need to deploy customized config files to the server then
-  # remove this.
- # :services_to_restart => %w(postfix sysklogd),
-  
-  # Set an email address to forward admin mail messages to. If you don't
-  # want to receive mail from the server (e.g. monit alert messages) then
-  # remove this.
-  :mail_forward_address => "dalef2005@yahoo.com.ar",
-  
-  # Set this if you want SSL to be enabled on the web server. The SSL cert 
-  # and key files need to exist on the server, The cert file should be in
-  # /etc/ssl/certs/default.pem and the key file should be in
-  # /etc/ssl/private/default.key (see :server_config_files_root).
-  :enable_ssl => true
-}
-
-
-
 set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
 set :rvm_install_ruby_params, '1.9.3 --with-gcc=clang'      # for jruby/rbx default to 1.9 mode
-set :rvm_install_pkgs, %w[iconv zlib libyaml openssl] # package list from https://rvm.io/packages
+#set :rvm_install_pkgs, %w[iconv libyaml openssl] # package list from https://rvm.io/packages
+set :rvm_install_pkgs, %w[] # package list from https://rvm.io/packages
 
-before 'deploy:setup', 'rvm:install_rvm'   # install RVM
-before 'deploy:setup', 'rvm:install_pkgs'  # install RVM packages before Ruby
-before 'deploy:setup', 'rvm:install_ruby'  # install Ruby and create gemset, or:
+
+#before 'deploy:setup', 'rvm:install_rvm'   # install RVM
+#before 'deploy:setup', 'rvm:install_pkgs'  # install RVM packages before Ruby
+#before 'deploy:setup', 'rvm:install_ruby'  # install Ruby and create gemset, or:
 before 'deploy:setup', 'rvm:create_gemset' # only create gemset
 before 'deploy:setup', 'rvm:import_gemset' # import gemset from file
+
+
+#############################################################
+#	Passenger
+#############################################################
+
+namespace :passenger do
+    desc "Restart Application"
+    task :restart do
+        run "touch #{current_path}/tmp/restart.txt"
+    end
+end
+
+after :deploy, "passenger:restart"
